b5a124938f3cd7a4c6f700cb8aff106abb4a0af6
xXrederXx <amorgenthalerthierry@gmail.com>
2025-01-23 19:36:20 +0100

    Add BaseTest

---
+++ Tests/BaseTest.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Numerics;
+using CsTkinter.Windows;
+
+namespace CsTkinter.Tests;
+
+public class BaseTest
+{
+    protected CtWindow app;
+    private readonly string TestId;
+
+    public BaseTest(string testId)
+    {
+        TestId = testId;
+        Console.WriteLine("Launching Test Window Id: " + TestId);
+
+        app = new CtWindow();
+
+        InitTest();
+
+        app.Run();
+
+        Console.WriteLine($"WPF Window Id: {TestId} closed.");
+    }
+
+    public virtual void InitTest()
+    {
+        System.Console.WriteLine("No InitTest defined for Window Id: " + TestId);
+    }
+}
